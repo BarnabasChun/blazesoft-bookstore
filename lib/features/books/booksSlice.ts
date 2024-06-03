afed3900cdfdc8d9c4f78446cfbfbf461db6a086
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-06-03 12:21:11 -0400

    Init redux store with books slice

---
+++ lib/features/books/booksSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../store'
+
+interface Book {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  description: string;
+}
+
+export interface BooksState {
+  list: Book[]
+}
+
+const initialState: BooksState = {
+  list: []
+}
+
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action:PayloadAction<Book>) => {
+      state.list.push(action.payload)
+    }
+  }
+})
+
+export const { addBook } = booksSlice.actions
+
+export const selectBooksList = (state: RootState) => state.books.list
+
+export default booksSlice.reducer
---
+++ lib/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import booksSlice from './features/books/booksSlice'
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      books: booksSlice
+    }
+  })
+}
+
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
