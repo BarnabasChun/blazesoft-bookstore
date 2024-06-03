f8e910a6df20ebefe0c91dc10205fedc254d9b66
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-06-03 15:52:33 -0400

    Create book form component

---
+++ app/components/book-form.tsx
@@ -0,0 +1,85 @@
+'use client';
+import { useEffect } from 'react';
+import { Book } from '@/lib/features/books/booksSlice';
+import { Button, Field, Fieldset, Label, Legend, Textarea } from '@headlessui/react';
+import { useForm } from 'react-hook-form';
+import InputField from "@/components/input-field";
+import clsx from 'clsx';
+
+export interface BookFormProps {
+  title: string;
+  onSubmit(formData: Book): void;
+}
+
+function BookForm({ title, onSubmit }: BookFormProps) {
+  const { register, handleSubmit, formState, reset } = useForm<Book>();
+  const { errors } = formState;
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState, reset]);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Fieldset className="space-y-6">
+        <Legend className="text-lg font-bold">{title}</Legend>
+
+        <InputField
+          label="Name"
+          isRequired
+          {...register("name", { required: "Please enter a name." })}
+          errorMessage={errors.name?.message}
+        />
+
+        <InputField
+          label="Price"
+          isRequired
+          {...register("price", {
+            required: "Please enter a price.",
+            valueAsNumber: true,
+            validate: (value) => {
+              if (isNaN(value)) {
+                return "Please enter a valid number.";
+              }
+
+              return value >= 0 || "Please enter a positive number.";
+            }
+          })}
+          errorMessage={errors.price?.message}
+        />
+
+        <InputField
+          label="Category"
+          isRequired
+          {...register("category", { required: "Please enter a category." })}
+          errorMessage={errors.category?.message}
+        />
+
+        <Field>
+          <Label className="block text-sm font-bold mb-2">Description</Label>
+          <Textarea
+            className={
+              clsx('shadow border mt-3 block w-full resize-none rounded-lg py-1.5 px-3 text-sm/6',
+                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25')
+            }
+            rows={3}
+            {...register("description", { required: false })}
+          />
+        </Field>
+      </Fieldset>
+
+      <div className="mt-6 flex justify-end">
+        <Button
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export default BookForm;
--- lib/features/books/booksSlice.ts
+++ lib/features/books/booksSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../../store';
 
-interface Book {
+export interface Book {
   id: number;
   name: string;
   price: number;
